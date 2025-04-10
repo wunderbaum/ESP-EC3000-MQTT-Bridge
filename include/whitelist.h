0474f577f1ffae6692b02fff4d2a91fada28a05c
Wunder Baum <phettsack@live.de>
2025-04-10 17:52:02 +0200

    Add files via upload

---
+++ include/whitelist.h
@@ -0,0 +1,14 @@
+// Whitelist EC3000-IDs
+
+#pragma once
+
+const char* whitelist[] = {
+  "BF8C",  // Trockner
+  "BACD",  // Waschmaschine
+  "BF3D",  // Geschirrspüler
+  "BE7A"   // Kühlschrank
+
+};
+
+const int whitelistSize = sizeof(whitelist) / sizeof(whitelist[0]);
+
---
+++ include/wifidata.h
@@ -0,0 +1,7 @@
+// WiFi and MQTT settings (EDIT THESE)
+const char* ssid = "";
+const char* password = "";
+const char* mqtt_server = "";
+const int mqtt_port = 1883;
+const char* mqtt_user = ""; // put your mqtt username here
+const char* mqtt_password = ""; // put your mqtt password here
