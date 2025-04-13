9d7a5fcc9ec8cf2eb6ac261fc254a974a5c4f1fd
Wunder Baum <phettsack@live.de>
2025-04-13 11:56:13 +0200

    Add files via upload

---
+++ include/fonts.h
@@ -0,0 +1,48 @@
+// Font array (modify as needed)
+// https://github.com/olikraus/u8g2/wiki/fntlist12#u8g2-fonts-capital-a-height-912
+const uint8_t* fonts[] = {
+    // unsorted
+    u8g2_font_ncenB10_tr,            // 0 - NULL !!!
+    u8g2_font_9x6LED_tr,             // 1
+    u8g2_font_spleen5x8_mu,          // 2
+    u8g2_font_NokiaSmallPlain_tf,    // 3
+    u8g2_font_BitTypeWriter_tr,
+    u8g2_font_PixelTheatre_tr,
+    u8g2_font_ImpactBits_tr,
+    u8g2_font_doomalpha04_tr,
+    u8g2_font_busdisplay11x5_tr,
+    u8g2_font_lastapprenticebold_tr,
+    u8g2_font_cube_mel_tr,           // 10
+    u8g2_font_press_mel_tr,
+    u8g2_font_repress_mel_tr,
+    u8g2_font_smart_patrol_nbp_tr,
+    u8g2_font_missingplanet_tr,
+    u8g2_font_ordinarybasis_tr,
+    u8g2_font_questgiver_tr,
+    u8g2_font_seraphimb1_tr,
+    u8g2_font_koleeko_tu,
+    u8g2_font_tenthinguys_tr,
+    u8g2_font_tenthinnerguys_tr,      // 20
+    u8g2_font_8bitclassic_tr,
+    u8g2_font_Terminal_tr,
+    // 10 high
+    u8g2_font_mademoiselle_mel_tr,
+    u8g2_font_pieceofcake_mel_tr,
+    u8g2_font_tenfatguys_tr,
+    u8g2_font_sonicmania_tr,
+    u8g2_font_DigitalDiscoThin_tr,
+    u8g2_font_DigitalDisco_tr,
+    u8g2_font_pxplusibmvga9_tr,
+    // 11 high
+    u8g2_font_michaelmouse_tu,         // 30
+    u8g2_font_squirrel_tr,
+    u8g2_font_fewture_tr,
+    u8g2_font_adventurer_tr,
+    u8g2_font_Pixellari_tr,
+    // 12 high
+    u8g2_font_cupcakemetoyourleader_tr,
+    u8g2_font_heavybottom_tr,
+    u8g2_font_12x6LED_tr,              // 37
+  };
+  const uint8_t numFonts = sizeof(fonts) / sizeof(fonts[0]);
+  
